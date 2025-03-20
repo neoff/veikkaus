1fe857a900ac5ed47f5f3a89a6ccb1b6686e1225
Evgenii Nesterov <en.varg@gmail.com>
2025-03-20 12:27:31 +0200

    #1 openapi contract

---
+++ src/test/java/fi/veikkaus/demo/contracts/BaseContractVerifierTest.java
@@ -0,0 +1,27 @@
+package fi.veikkaus.demo.contracts;
+
+import io.restassured.module.webtestclient.RestAssuredWebTestClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@AutoConfigureMessageVerifier
+public class BaseContractVerifierTest {
+  
+  @Autowired
+  private WebTestClient webTestClient;
+  
+  @BeforeEach
+  void setup() {
+    RestAssuredWebTestClient.webTestClient(webTestClient);
+  }
+}
